091a2d5538936b0059c1672f241fff99282b39d4
Miroslav I <styylzeh@gmail.com>
2021-11-10 09:51:47 +0200

    [6]: get data by using API & render data in html

--- js/components/ApartmentGridComponent.js
+++ js/components/ApartmentGridComponent.js
@@ -6,7 +6,31 @@ class ApartmentGridComponent {
     this.init();
   }
 
+  saveAparts = (apartaments) => {
+    this.state = { apartaments };
+    this.render();
+  };
+
+  showError = (error) => {
+    alert(error);
+  };
+
+  fetchAparts = () => {
+    API.fetchAparts(this.saveAparts, this.showError);
+  };
+
   init = () => {
+    this.fetchAparts();
     this.htmlElement = document.createElement("div");
+
+    this.render();
+  };
+
+  render = () => {
+    if (this.state.apartaments.length === 0) {
+      this.htmlElement.innerHTML = "siunčiama🐎";
+    } else {
+      this.htmlElement.innerHTML = "parsiųsta!";
+    }
   };
 }
